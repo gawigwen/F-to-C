b5c9ee7b9616421da6aa6485af01bbbb9abfbc73
gawigwen <19rodnce@waipahuh.k12.hi.us>
2017-11-07 17:11:30 -1000

    2

---
+++ exercises.js
@@ -0,0 +1,43 @@
+
+var a = 32
+var b = 1.8
+var c = 20
+var d = 40
+var e = 60
+var f = 80 
+var g = 100
+var h = 120
+
+function convert (a ,b ,c){
+	return (c - a) / b
+}
+console.log((c - a) / b);
+
+function convert (a ,b ,d){
+	return (d - a) / b
+}
+console.log((d - a) / b);
+
+function convert (a ,b ,e){
+	return (e - a) / b
+}
+console.log((e - a) / b);
+
+function convert (a ,b ,f){
+	return (f - a) / b
+}
+console.log((f - a) / b);
+
+function convert (a ,b ,g){
+	return (g - a) / b
+}
+console.log((g - a) / b);
+
+function convert (a ,b ,h){
+	return (h - a) / b
+}
+console.log((h - a) / b);
+
+
+
+
